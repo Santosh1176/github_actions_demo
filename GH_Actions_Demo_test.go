0f652b6ae1052e31cab5ed73da42a1c136b52697
Santosh Kaluskar <dtshbl@gmail.com>
2022-12-29 22:20:28 +0530

    testing gh action
    Signed-off-by: Santosh Kaluskar <dtshbl@gmail.com>

--- GH_Actions_Demo_test.go
+++ GH_Actions_Demo_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGH_Actions(t *testing.T) {
 	got := GH_Actions_Demo()
-	want := "It's amazing learning Github Actions with #90DaysOfdevOps"
+	want := "It's amazing learning Github Actions as a CI/CD tool with #90DaysOfdevOps"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
